40c5543e0ac57cc1a18c48973b6340e129c228e9
Beatriz Salles <beatrizsallesss@gmail.com>
2020-08-21 12:49:17 -0300

    Documents options.

--- src/index.js
+++ src/index.js
@@ -8,67 +8,102 @@ import './index.css';
 
 const steps = [
   {
-    id:'saudacao',
-    message: "Olá, eu sou o Caio. Sou membro virtual do CABICT.",
-    trigger:'perguntar nome',
+    id:"apresentacao",
+    message: "Olá, eu sou Caio, membro virtual do CABICT.",
+    trigger:"pergunta-nome",
   },
   {
-    id:"perguntar nome",
+    id:"pergunta-nome",
     message:"Qual seu nome?",
-    trigger: "esperando nome",
+    trigger:"nome",
   },
   {
-    id:"esperando nome",
-    user: true,
-    trigger: "agradecimentos",
+    id:"nome",  // Validação de nome
+    user:true, // Nome.capitalize()
+    trigger:"saudacao",
   },
   {
-    id: "agradecimentos",
-    message: "Olá, {previousValue}. Prazer em te conhecer!!",
-    trigger: "ajuda",
+    id:"saudacao",
+    message:"Olá, {previousValue}. Prazer em te conhecer!",  // Joke: se nome igual ao de Caio => "E ai, xará!"
+    trigger:"ver-cracha",
   },
   {
-    id: "ajuda",
-    message: "Em que posso ajudar?",
-    trigger: "opcoes de ajuda",
+    id:"ver-cracha",
+    message:"Quer ver meu crachá?",
+    trigger:"ver-cracha-opcoes",
   },
   {
-    id:"opcoes de ajuda",
+    id:"ver-cracha-opcoes",
     options: [
-      {value:1, label:"Documentos do curso", trigger:"documentos"},
-      {value:2, label:"ajuda", trigger:"ajuda"},
+      { value:"sim", label:"Sim", trigger:"ver-cracha-sim" },
+      { value:"nao", label:"Não", trigger:"oferece-ajuda"},
     ],
   },
   {
-    id: "documentos",
-    message: "Qual documento você quer?",
-    trigger: "listar-documentos",
+    id:"ver-cracha-sim",
+    message:"Aqui está meu crachá.", // Colocar imagem 
+    trigger: "oferece-ajuda",
   },
   {
-    id: "listar-documentos",
+    id:"oferece-ajuda",
+    message:"Em que posso ajudar?",
+    trigger:"oferece-ajuda-opcoes",
+  },
+  {
+    id:"oferece-ajuda-opcoes",
     options: [
-      {value:"ppc", label: "PPC do curso", trigger:"link-ppc"},
-      {value:"atividades-complementares", label:"Atividades complementares", trigger: "link-atividades-complementares"},
-      {value:"tabela-aproveitamento", label:"Tabela de aproveitamentos", trigger: "link-tabela-aproveitamento"},
+      { value:"documentos", label:"Documentos do curso", trigger:"documentos-curso" },
+      { value:"duvidas", label:"Dúvidas frequentes", trigger:"duvidas-frequentes" },
+      { value:"contatos", label:"Contatos da Coordenação", trigger:"contatos-coordenacao" },
+      { value:"nenhum", label:"Nenhuma opção", trigger:"nenhuma-opcao" },
     ],
   },
   {
-    id:"link-ppc",
-    component: (<div>Clique aqui para acessar o PPC do curso: <a href="http://bict.ufma.br/wp-content/uploads/2017/11/PPC_BICT.pdf" target="_blank">PPC do curso</a></div>),
-    asMessage: true,
-    end:true,
+    id:"documentos-curso",
+    message:"Estes são os documentos do curso. Qual você quer?",
+    trigger: "documentos-curso-opcoes",
   },
   {
-    id:"link-atividades-complementares",
-    component: (<div>Clique aqui para acessar às atividade complementares: <a href=" http://bict.ufma.br/wp-content/uploads/2019/11/NORMAS-2019.pdf" target="_blank">Atividades complementares</a></div>),
-    asMessage:true,
-    end:true,
+    id: "documentos-curso-opcoes",
+    options: [
+      { value:"ppc", label:"PPC do curso", trigger: "ppc-link"},
+      { value:"atividades-complementares", label:"Atividades complementares", trigger: "atividades-complementares-link"},
+      { value:"quadro-aproveitamento", label:"Quadro de aproveitamento", trigger: "quadro-aproveitamento-link"},
+      { value:"nenhum", label:"Nenhuma opção", trigger: "nenhuma-opcao"},
+    ],
   },
   {
-    id:"link-tabela-aproveitamento",
-    component: (<div>Clique aqui para acessar ao Quadro de aproveitamento: <a href="http://bict.ufma.br/wp-content/uploads/2019/08/QUADRO-DE-EQUIVAL%C3%8ANCIA-P-APROVEITAMENTOS-2019-15-08-2019.pdf" target="_blank">Quadro de Aproveitamento</a></div>),
+    id:"ppc-link",
+    component: (<div>Clique no link para acessar o <a href="http://bict.ufma.br/wp-content/uploads/2017/11/PPC_BICT.pdf" target="_blank" style={{ color: '#FFF'}}>PPC do curso</a></div>),
     asMessage: true,
-    end:true,
+    end: true,
+  },
+  {
+    id:"atividades-complementares-link",
+    component: (<div>Clique no link para acessar as <a href="http://bict.ufma.br/wp-content/uploads/2019/11/NORMAS-2019.pdf" target="_blank" style={{ color: '#FFF'}}>Atividades complementares</a></div>),
+    asMessage: true,
+    end: true,
+  },
+  {
+    id:"quadro-aproveitamento-link",
+    component: (<div>Clique no link para acessar o <a href="http://bict.ufma.br/wp-content/uploads/2019/08/QUADRO-DE-EQUIVAL%C3%8ANCIA-P-APROVEITAMENTOS-2019-15-08-2019.pdf" target="_blank" style={{ color: '#FFF'}}>Quadro de aproveitamento</a></div>),
+    asMessage: true,
+    end: true,
+  },
+  {
+    id:"duvidas-frequentes",
+    message:"Escolha uma opção.",
+    end: true,
+  },
+  {
+    id:"contatos-coordenacao",
+    message:"Estes são os meios de contato com a Coordenação.",
+    end: true,
+  },
+  {
+    id:"nenhuma-opcao",
+    message: "Sinto muito, não possuo a informação que procura. >_<", // Colocar emojis
+    end: true,
   },
 ];
 
